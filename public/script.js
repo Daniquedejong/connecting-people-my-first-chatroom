6b530f65165ffb2efd457769d81ac4958b0ede33
Daniquedejong <danique.de.jong3@hva.nl>
2023-05-30 19:52:30 +0200

    v2

--- public/script.js
+++ public/script.js
@@ -1,29 +1,67 @@
-let socket = io();
-let messages = document.querySelector("section ul");
-let input = document.querySelector("input");
+let socket = io()
+let messages = document.querySelector('section ul')
+let input = document.querySelector('input')
+let naam = document.querySelector('#naam')
+let bericht = document.querySelector('#bericht')
+let form = document.querySelector('form')
 
-document.querySelector("form").addEventListener("submit", (event) => {
-  event.preventDefault();
+const logo = document.querySelector('.logo')
+
+
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault()
   if (input.value) {
-    socket.emit("message", input.value);
-    input.value = "";
+    socket.emit('message', {
+    naam: naam.value,
+    bericht: bericht.value
+  }),
+    bericht.value = ''
   }
-});
-socket.on("message", (message) => {
-  addMessage(message);
-});
-socket.on("whatever", (message) => {
-  addMessage(message);
-});
-socket.on("history", (history) => {
+})
+
+socket.on('message', (message) => { 
+  addMessage(`${message.naam}: ${message.bericht}`)
+  
+})
+
+socket.on('whatever', (message) => {
+  addMessage(message)
+})
+
+// Hiermee worden het aantal connecties bijgewerkt
+socket.on('connectionCount', (count) => {
+  const connectionCountElement = document.querySelector('#connection-count')
+  if (connectionCountElement) {
+    connectionCountElement.textContent = `Personen aanwezig: ${count}`
+  }
+
+  logo.classList.add('animatie')
+
+  // Verwijder de 'animatie' klasse na een korte vertraging
+  setTimeout(() => {
+    logo.classList.remove('animatie');
+  }, 1000); // Verander de vertraging (in milliseconden) naar jouw wens
+})
+
+socket.on('history', (history) => {
   history.forEach((message) => {
-    addMessage(message);
-  });
-});
+    addMessage(`${message.naam}: ${message.bericht}`)
+  })
+})
 
 function addMessage(message) {
-  messages.appendChild(
-    Object.assign(document.createElement("li"), { textContent: message })
-  );
-  messages.scrollTop = messages.scrollHeight;
+  const currentTime = new Date().toLocaleTimeString('nl-NL', { hour: 'numeric', minute: 'numeric' });
+ 
+  const messageElement = document.createElement('li');
+  const timeElement = document.createElement('span');
+
+  messageElement.classList.add('own-message')
+  timeElement.classList.add('own-time')
+
+  messages.appendChild(Object.assign(messageElement, { textContent: message }))
+  messages.appendChild(Object.assign(timeElement, { textContent: currentTime }));
+  messages.scrollTop = messages.scrollHeight
+
+ 
 }
--- server.js
+++ server.js
@@ -1,24 +1,94 @@
-import * as path from "path";
+/* Losjes gebaseerd op https://socket.io/get-started/chat */
 
-import { Server } from "socket.io";
-import { createServer } from "http";
-import express from "express";
+import * as path from 'path'
+import { Server } from 'socket.io'
+import { createServer } from 'http'
+import express from 'express'
 
-const app = express();
-const http = createServer(app);
-const ioServer = new Server(http);
-const port = process.env.PORT || 4242;
+const app = express()
+const http = createServer(app)
+const ioServer = new Server(http)
+const port = process.env.PORT || 8000
 
-app.use(express.static(path.resolve("public")));
+// Om de history te kunnen zien
+const historySize = 50
 
-ioServer.on("connection", (client) => {
-  console.log(`user ${client.id} connected`);
+let history = []
 
-  client.on("message", (message) => {
-    ioServer.emit("message", message);
-  });
-});
 
+// Om de connecties bij te houden
+let connections = 0
+
+
+
+// Serveer client-side bestanden
+app.use(express.static(path.resolve('public')))
+
+
+
+
+// Start de socket.io server op
+ioServer.on('connection', (client) => {
+  // Tel er 1 connectie bij op
+  connections++
+ 
+  // Log de connectie naar console
+  console.log(`user ${client.id} connected, total connections ${connections}`)
+
+    // Stuur het aantal connecties naar alle clients
+    ioServer.emit('connectionCount', connections);
+
+
+  // Stuur de historie door, let op: luister op socket, emit op io!
+  ioServer.emit('history', history)
+
+  // Luister naar een message van een gebruiker
+  client.on('message', (message) => {
+
+    // Log het ontvangen bericht
+    console.log(`user ${client.id} sent message: ${message}`)
+
+     // Check de maximum lengte van de historie
+     while (history.length > historySize) {
+      history.shift()
+    }
+    // Voeg het toe aan de historie
+    history.push(message)
+
+    // Verstuur het bericht naar alle clients
+    ioServer.emit('message', message)
+  })
+
+  // Luister naar een disconnect van een gebruiker
+  client.on('disconnect', () => {
+    // haal 1 connectie eraf
+    connections--
+    // Log de disconnect
+    console.log(`user ${client.id} disconnected, total connections: ${connections}`)
+
+      // Stuur het aantal connecties naar alle clients
+    ioServer.emit('connectionCount', connections);
+
+  })
+})
+
+// Maak een route voor de index 
+app.get('/', (request, response) => {
+ 
+  fetchJson().then((data) => {
+    response.render('index')
+  })
+})
+
+    // definieer de fetchJson functie
+    async function fetchJson(url) {
+      return await fetch(url)
+        .then((response) => response.json())
+        .catch((error) => error)
+    }
+
+
+// Start een http server op het ingestelde poortnummer en log de url
 http.listen(port, () => {
-  console.log("listening on http://localhost:" + port);
-});
+  console.log('listening on http://localhost:' + port)
+})
